f9ac4266814bb7b86fa2ecebab66ea0c111805fc
burabury <bura.bury@gmail.com>
2021-03-15 15:24:31 +0100

    security config fixed

--- src/main/java/com/example/springbootthymeleafcrudwebbapp/config/WebSecurityConfig.java
+++ src/main/java/com/example/springbootthymeleafcrudwebbapp/config/WebSecurityConfig.java
@@ -31,9 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasRole("USER")
+                .antMatchers("/").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
                 .and()
